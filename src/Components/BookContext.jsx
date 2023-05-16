45409be642da997de37767bd829f2306baf6eddc
Leon-Masakuna <masakunamfengleon@gmail.com>
2023-05-16 12:01:55 +0200

    feat: implemente react context

--- src/Components/BookContext.jsx
+++ src/Components/BookContext.jsx
@@ -1,30 +1,29 @@
-import React, {createContext, useState} from "react";
+import React, { createContext, useState } from "react";
 
 export const BooklistContext = createContext();
 
-export const BooklistProvider = ({children}) => {
-    const [books, setBooks] = useState([
-   
-        {
-            id : 1,
-            name : 'Livre1',
-            author : 'Héritier'
-        },
-        {
-            id : 2,
-            name : 'livre2',
-            author : 'Sylvain'
-        },
-        {
-            id : 3,
-            name : 'livre3',
-            author : 'Gloriane'
-        }
-    ])
+export const BooklistProvider = ({ children }) => {
+  const [books, setBooks] = useState([
+    {
+      id: 1,
+      name: "MySQL 5",
+      author: "Cyril Thibaud",
+    },
+    {
+      id: 2,
+      name: "Merise",
+      author: "Laurent Audibert",
+    },
+    {
+      id: 3,
+      name: "SQL Server 2008",
+      author: "Jérôme Gabillaud",
+    },
+  ]);
 
-    return(
-        <BooklistContext.Provider value={[books, setBooks]}>
-            {children}
-        </BooklistContext.Provider>
-    )
-}
+  return (
+    <BooklistContext.Provider value={[books, setBooks]}>
+      {children}
+    </BooklistContext.Provider>
+  );
+};
--- src/Components/Booklist.jsx
+++ src/Components/Booklist.jsx
@@ -1,24 +1,21 @@
-import React, {useContext } from 'react'
-import {BooklistContext} from "./BookContext"
+import React, { useContext } from "react";
+import { BooklistContext } from "./BookContext";
 
 function Booklist() {
-    const [books, setBooks] = useContext(BooklistContext)
-    
-    return (
-        <div>
-            <h1>Liste de livres</h1>
-            {
-                books.map(book => (
-                    <div key={book.id}>
-                        <h3> {book.name} </h3>
-                        <span> {book.author} </span>
-                        <hr/>
-                    </div>
-                ))
-            }
-        </div>
-    )
+  const [books, setBooks] = useContext(BooklistContext);
 
+  return (
+    <div>
+      <h1>Liste de livres</h1>
+      {books.map((book) => (
+        <div key={book.id}>
+          <h3> {book.name} </h3>
+          <span> {book.author} </span>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Booklist
+export default Booklist;
